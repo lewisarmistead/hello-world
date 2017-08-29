c1b0ba9742443f43a320889d186edf34c0f8e9a3
lewis.armistead <lewis.armistead@apexcleanenergy.com>
2017-08-29 09:58:19 -0400

    added yellow and orange to list

--- colors.py
+++ colors.py
@@ -1,4 +1,4 @@
-myList = ['red', 'green', 'blue']
+myList = ['red', 'green', 'blue', 'yellow', 'orange']
 
 for color in myList:
     print 'The word "%s" has %s characters!!!!!!' % (color,len(color))
